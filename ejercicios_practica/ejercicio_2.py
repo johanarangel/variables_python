1e2879f55440ba9cf7ceb7c7d7c506965f2c17e2
Muntaok <64455138+Muntaok@users.noreply.github.com>
2021-07-14 12:29:31 -0300

    Ejercicio Nº 2

--- ejercicios_practica/ejercicio_2.py
+++ ejercicios_practica/ejercicio_2.py
@@ -20,6 +20,9 @@ numero_2 = int(input())
 # Alumno: Imprima en pantalla los dos números decimales solicitados
 # print(....)
 
+print("El primer numero es", numero_1)
+print("El segundo numero es", numero_2)
+
 # Alumno: Calcule la suma, resta, división y multiplicación de los números ingresados
 # numero_1, numero_2
 # Imprima en pantalla todos los resultados con el siguiente formato de ejemplo:
@@ -27,9 +30,18 @@ numero_2 = int(input())
 # NOTA: No coloque usted los nùmeros y resultados, use las variables
 
 # Suma
+suma = numero_1 + numero_2
+print("El resultado de sumar", numero_1, " y ", numero_2, " es ", suma)
 
 # Resta
+resta = numero_1 - numero_2
+print("El resultado de restar", numero_1, " y ", numero_2, " es ", resta)
 
 # División
+division = numero_1 // numero_2
+print("El resultado de dividir", numero_1, " y ", numero_2, " es ", division)
 
 # Multiplicación
+multiplicacion = numero_1 * numero_2
+print("El resultado de multiplicar", numero_1,
+      " y ", numero_2, " es ", multiplicacion)
