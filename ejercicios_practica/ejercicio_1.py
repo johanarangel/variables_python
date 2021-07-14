ad4f77391a93cccc2a8c8913dc4430d5700e7659
Muntaok <64455138+Muntaok@users.noreply.github.com>
2021-07-14 19:17:27 -0300

    Resolucion Ejercicio Nº 1

--- ejercicios_practica/ejercicio_1.py
+++ ejercicios_practica/ejercicio_1.py
@@ -12,17 +12,17 @@
 numero_1 = 5
 numero_2 = 7
 
-operacion = numero_1 + numero_2
-
-print("El resultado de la suma es", operacion)
-
 # Realizar la suma de las dos variables
 # numero_1 y numero_2
 # Almacenar el valor de la suma en una variable
 # ej:
 # operacion = .....
+suma = numero_1 + numero_2
 
 # Imprimir en pantalla el resultado de la suma
 # print(....)
+print("El resultado de la suma es", suma)
 
 # Repita el procedimiento para realizar la resta
+resta = numero_1 - numero_2
+print("El resultado de la resta es", resta)
