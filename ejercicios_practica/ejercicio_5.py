acdff1204588283e6efc950b11091ce41020643c
Muntaok <64455138+Muntaok@users.noreply.github.com>
2021-07-14 16:35:45 -0300

    Resolucion Ejercicio Nº 5

--- ejercicios_practica/ejercicio_5.py
+++ ejercicios_practica/ejercicio_5.py
@@ -17,6 +17,13 @@ print('Ingrese palabra 2:')
 palabra_2 = str(input())
 
 # De la primera palabra tome las primeras tres letras, utilice el operador :
+palabra_1_acronimo = palabra_1[:3]
+
 # De la segunda palabra tome las primeras dos letras, utilice el operador :
+palabra_2_acronimo = palabra_2[:2]
+
 # Formar una nueva palabra con los recortes solicitados
+palabra_nueva = palabra_1_acronimo + palabra_2_acronimo
+
 # Imprima en pantalla los resultados
+print(palabra_nueva)
