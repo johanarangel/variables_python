d6612a7f264f7f9337099aa17f0263ef8e41687c
johanarangel <68509096+johanarangel@users.noreply.github.com>
2021-07-17 19:19:27 -0300

    Update ejercicio_3.py

--- ejercicios_practica/ejercicio_3.py
+++ ejercicios_practica/ejercicio_3.py
@@ -26,5 +26,7 @@ nombre_completo = nombre + apellido
 
 # Imprimir la cantidad de letras que posee su nombre completo
 # cantidad_letras = len(....)
-cantidad_letras = len(nombre_completo)
-print(len(cantidad_letras))
+cantidad_letras = len(nombre_completo) 
+print(len(cantidad_letras)) #Esto te da un error que dice "Object of type int has no len()", esto significa 
+                            #que la variable cantidad_letras es el número de carácteres que tiene el nombre completo
+                            #y si vuelves a usar len(cantidad_letras) alli salta el error porque la función len() es para string no enteros int
